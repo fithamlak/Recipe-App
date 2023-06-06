dc0e83f5f92799be1a399adda32fdd3eeaabfd85
fithamlak <justiceofgod01@gmail.com>
2023-06-06 17:22:24 +0300

    fix rubocop error

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,36 +1,36 @@
 class FoodsController < ApplicationController
-    def index
-      @foods = Food.all
-    end
-  
-    def new
-      @food = Food.new
-    end
-  
-    def create
-      @food = Food.new(foods_params)
-  
-      if @food.valid?
-        @food.save
-        flash[:notice] = 'successfully added'
-        redirect_to foods_path
-      else
-        flash[:alert] = 'not added'
-        @error = @food.errors.full_messages
-        render :new
-      end
-    end
-  
-    def destroy
-      food = Food.find(params[:id])
-      food.destroy
-      flash[:notice] = 'Food has been deleted!'
+  def index
+    @foods = Food.all
+  end
+
+  def new
+    @food = Food.new
+  end
+
+  def create
+    @food = Food.new(foods_params)
+
+    if @food.valid?
+      @food.save
+      flash[:notice] = 'successfully added'
       redirect_to foods_path
-    end
-  
-    private
-  
-    def foods_params
-      params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user: current_user)
+    else
+      flash[:alert] = 'not added'
+      @error = @food.errors.full_messages
+      render :new
     end
   end
+
+  def destroy
+    food = Food.find(params[:id])
+    food.destroy
+    flash[:notice] = 'Food has been deleted!'
+    redirect_to foods_path
+  end
+
+  private
+
+  def foods_params
+    params.require(:food).permit(:name, :measurement_unit, :price, :quantity).merge(user: current_user)
+  end
+end
