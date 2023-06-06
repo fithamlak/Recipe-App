09fd381f790b7a055873c168b1e1dc4bc15eb01d
fithamlak <justiceofgod01@gmail.com>
2023-06-06 13:48:09 +0300

    Finish generating devise views and other configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'users#index'
+  resources :users do
+    resources :foods, only: [:index, :create, :destroy]
+    resources :recipes, only: [:index, :create, :destroy, :show]
+  end
 end
